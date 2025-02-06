a515295cc98bb178ef4243e2d4e1396e7227f9a8
unknown <harshprajapati212@gmail.com>
2025-02-06 16:23:37 +0530

    updated

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -12,15 +12,22 @@ function About() {
                         <div className="title">
                                 <span>About Me</span>
                                 
-                                <h2> A passionate web designer turning ideas into visually tunning, user-friendly websites.</h2>
-                            </div>
-                            <p>Having 3 years of Experience as a UI/UX Designer with a passion for crafting intuitive and visually
+                                {/* <h2> A passionate web designer turning ideas into visually tunning, user-friendly websites.</h2> */}
+                                <h2>Having 3 years of Experience as a UI/UX Designer with a passion for crafting intuitive and visually
                                 appealing digital experiences. Creating responsive, visually appealing websites. Proficient in HTML, CSS,
                                 JavaScript, Adobe Creative Suite, wireframes, mockups, and prototypes to communicate design concepts
                                 effectively. Skilled in conducting user research, gathering feedback, and iterating designs based on
                                 insights. Strong advocate for user-centered design principles and processes. Collaborative team player with
                                 excellent communication and problem-solving skills. Ready to contribute to creating innovative solutions
-                                that delight users and drive business success.</p>
+                                that delight users and drive business success.</h2>
+                            </div>
+                            {/* <p>Having 3 years of Experience as a UI/UX Designer with a passion for crafting intuitive and visually
+                                appealing digital experiences. Creating responsive, visually appealing websites. Proficient in HTML, CSS,
+                                JavaScript, Adobe Creative Suite, wireframes, mockups, and prototypes to communicate design concepts
+                                effectively. Skilled in conducting user research, gathering feedback, and iterating designs based on
+                                insights. Strong advocate for user-centered design principles and processes. Collaborative team player with
+                                excellent communication and problem-solving skills. Ready to contribute to creating innovative solutions
+                                that delight users and drive business success.</p> */}
                         </div>
                     </div>
                 </div>
