f886462d900e3f1e6c81803d7abaf3577c770d04
unknown <harshprajapati212@gmail.com>
2025-02-14 11:36:51 +0530

    updadet

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -1,10 +1,24 @@
 
 function Banner() {
+
+    const handleDownload = () => {
+        const pdfPath = "/images/resume-latest.pdf"; // Path to your PDF in the public folder
+
+        // Create an anchor element and trigger the download
+        const link = document.createElement("a");
+        link.href = pdfPath;
+        link.download = "resume-latest.pdf"; // Rename the file on download
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
+
     return (
         <>
             <section id="home" className="banner_wrap" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">
                 <div className="lineshape">
-                <img src="images/line_shape.png" className="img-fluid" alt="shape" />
+                    <img src="images/line_shape.png" className="img-fluid" alt="shape" />
                 </div>
                 <div className="shape2">
                     <img src="images/shape1.png" className="img-fluid" alt="shape" />
@@ -37,11 +51,14 @@ function Banner() {
 
                         <div className="col-sm-6">
                             <div className="banner_info">
-                            <h1 className="text-anime-style-2">Hello, I am <span>Harsh</span></h1>
+                                <h1 className="text-anime-style-2">Hello, I am <span>Harsh</span></h1>
                                 <h2>UI/UX Designer and
                                     Frontend Developer</h2>
                                 <p>I have been working, i am proficient in ui design, user experience, researcher, web development.</p>
-                                <a href="" className="btn btn-primary">Download CV <img src="images/download.svg" alt="download" /></a>
+                                <button className="btn btn-primary" onClick={handleDownload} >
+                                    Download Resume
+                                </button>
+                                {/* <a href="" >Download CV <img src="images/download.svg" alt="download" /></a> */}
                             </div>
                         </div>
                     </div>
