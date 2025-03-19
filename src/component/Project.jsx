178a335676646683a02cb587ba54b89d96def44a
unknown <harshprajapati212@gmail.com>
2025-03-19 13:04:21 +0530

    Your commit message

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -11,14 +11,14 @@ function Project() {
             tag1: "WEBSITE",
             tag2: "APP",
             image: "../images/Hotel-Booking.png",
-            link:"https://www.behance.net/gallery/188043731/Hotel-Booking-App"
+            link: "https://www.behance.net/gallery/188043731/Hotel-Booking-App"
         },
         {
             id: 2,
             title: "Food Delivery App",
             tag1: "APP",
             image: "../images/FoodDelivery.png",
-            link:"https://www.behance.net/gallery/196974201/Food-Delivery-App"
+            link: "https://www.behance.net/gallery/196974201/Food-Delivery-App"
 
         }
         ,
@@ -27,14 +27,14 @@ function Project() {
             title: "News Website",
             tag1: "WEBSITE",
             image: "../images/News-Website.png",
-            link:"https://www.behance.net/gallery/210502495/Journal-Of-Supply-Chain-Media-Website"
+            link: "https://www.behance.net/gallery/210502495/Journal-Of-Supply-Chain-Media-Website"
         },
         {
             id: 4,
             title: "Petroleum Company Website",
             tag1: "WEBSITE",
             image: "../images/Petroleum-Company.png",
-            link:"https://www.behance.net/gallery/212860079/Petroleum-Company-Website"
+            link: "https://www.behance.net/gallery/212860079/Petroleum-Company-Website"
 
         },
         {
@@ -42,7 +42,7 @@ function Project() {
             title: "HRMS",
             tag1: "APP",
             image: "../images/HRMS.png",
-            link:"https://www.behance.net/gallery/196750573/HRMS-App"
+            link: "https://www.behance.net/gallery/196750573/HRMS-App"
         }
         ,
         {
@@ -50,9 +50,38 @@ function Project() {
             title: "Go Kamp",
             tag1: "WEBSITE",
             image: "../images/Gokamp.png",
-            link:"https://www.behance.net/gallery/188425617/Camping-website"
+            link: "https://www.behance.net/gallery/188425617/Camping-website"
 
         }
+       ,
+        {
+            id: 6,
+            title: "Jetrise",
+            tag1: "WEBSITE",
+            image: "../images/jetrise.jpg",
+            link: "https://www.behance.net/gallery/209909287/Jetrise-Private-Jet-Booking-Landing-Page"
+        },
+        {
+            id: 7,
+            title: "India Supply Chain Summit",
+            tag1: "WEBSITE",
+            image: "../images/indiasupply.png",
+            link: "https://www.behance.net/gallery/210504819/India-Supply-Chain-Summit-Conference-Event-Website"
+        },
+        {
+            id: 8,
+            title: "Stationary Shop Website",
+            tag1: "WEBSITE",
+            image: "../images/secodi.png",
+            link: "https://www.behance.net/gallery/196753235/Stationary-Shop-WebsiteF"
+        },
+        {
+            id: 9,
+            title: "Gateway Of Asia",
+            tag1: "WEBSITE",
+            image: "../images/incorporation.png",
+            link: "https://www.behance.net/gallery/193456033/Incorporation-Service"
+        },
     ]
 
     const Modaldata = [
@@ -86,22 +115,22 @@ function Project() {
                     <div className="row">
                         {
                             projects.map((projects) => (
-                               
-                                    <div className="col-sm-12 col-md-6 col-lg-4">
-                                        <div className="project_box" data-aos="flip-left" data-aos-duration="1000" data-aos-offset="100" data-aos-delay="200" data-aos-once="true">
-                                            <div className="project_img">
-                                                <a type="button" href={projects.link}><img src={projects.image} className="img-fluid" alt="project" /></a>
-                                            </div>
 
-                                            <div className="info">
-                                                <div className="tags">
-                                                    <span>{projects.tag1}</span>
-                                                </div>
-                                                <h4><a className="text-white" href={projects.link}>{projects.title}</a></h4>
+                                <div className="col-sm-12 col-md-6 col-lg-4">
+                                    <div className="project_box" data-aos="flip-left" data-aos-duration="1000" data-aos-offset="100" data-aos-delay="200" data-aos-once="true">
+                                        <div className="project_img">
+                                            <a type="button" href={projects.link}><img src={projects.image} className="img-fluid" alt="project" /></a>
+                                        </div>
+
+                                        <div className="info">
+                                            <div className="tags">
+                                                <span>{projects.tag1}</span>
                                             </div>
+                                            <h4><a className="text-white" href={projects.link}>{projects.title}</a></h4>
                                         </div>
                                     </div>
-                                
+                                </div>
+
                             )
                             )
                         }
